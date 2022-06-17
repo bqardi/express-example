ef3e70784939f955a986c25c0bc21e1c2d28a1da
Sune Seifert <sse@abtion.com>
2022-06-17 13:01:30 +0200

    Delete SSL

--- query.js
+++ query.js
@@ -2,8 +2,7 @@ const Pool = require('pg').Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: process.env.NODE_ENV === "production"
+	connectionString: process.env.DATABASE_URL
 });
 
 function find(req, res) {
